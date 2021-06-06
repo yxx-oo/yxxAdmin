206488152ed676ea2bc44cce7b5dbf99b0fd4e85
yxx <447112751@qq.com>
2021-06-06 18:44:41 +0800

    [yxxAdmin][主工程][yxx][R:] 订单页面下拉框开发

--- src/views/orderManager/components/OrderManagerHeader.vue
+++ src/views/orderManager/components/OrderManagerHeader.vue
@@ -1,15 +1,49 @@
 <template>
   <div class="order-manager-header">
-    ddd
+    <el-cascader
+      v-model="selectValue"
+      :options="data"
+      :show-all-levels="false"
+      filterable
+      :props="{ expandTrigger: 'hover', value: 'id', label: 'certificationProgram' }"
+      :filter-method="filterMethod"
+    >
+    </el-cascader>
   </div>
 </template>
 
 <script>
 // import openWindow from '@/utils/open-window'
+import requedata from '../config.json'
 
 export default {
   name: 'OrderManagerHeader',
-  methods: {
+  data() {
+    return {
+      selectValue,
+      data
+    }
+  },
+  watch: {
+  },
+  mothods: {
+    filterMethod(val) {
+      this.value = val;
+      if (val) { //val存在
+        this.data = this.optionsCopy.filter((item) => {
+          if (!!~item.label.indexOf(val) || !!~item.label.toUpperCase().indexOf(val.toUpperCase())) {
+            return true
+          }
+        })
+      } else { //val为空时，还原数组
+        // this.data = this.optionsCopy;
+      }
+    }
+  },
+  created() {
+    this.data = requedata;
+  },
+  ethods: {
 
   }
 }
